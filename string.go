5039cd120103843ca394f9948bd2822ab3d58050
Alfahan <ali.farhan160@gmail.com>
2022-09-05 10:16:09 +0700

    Type Data String

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Alfa")
+	fmt.Println("Ali Farhan")
+	fmt.Println("Mohamad Ali Farhan")
+
+	fmt.Println(len("Alfa"))
+	fmt.Println("Ali Farhan"[0])
+	fmt.Println("Mohamad Ali Farhan"[1])
+}
